eaa9abdcba3f6a7003a9c32cbd41e6428acbabfd
taksahi <takashi4233@gmail.com>
2021-02-16 21:19:42 +0900

    数当てゲーム完成 ・エラー処理(expect)メソッドから、match式に切り替える。 ・parseメソッドはResult型を返し、OkかErrの列挙子を取りうる列挙型

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,21 +5,29 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
+	println! ("Guess the number!");
 	let secret_number = rand::thread_rng().gen_range(1,101);
 	
-    println!("The secret numbre is:{}",secret_number);
-	 println!("Please input your guess.");
-	 let mut guess = String::new();	//Rustでは、変数は標準で不変。mutをつけて可変にする。
 
-	 io::stdin().read_line(&mut guess).expect("Failed to read line");
+	loop {
+		  println!("Please input your guess.");
+	 	  let mut guess = String::new();	//Rustでは、変数は標準で不変。mutをつけて可変にする。
 
-	 let guess: u32 = guess.trim().parse().expect("Please type a number!");
+		  io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-	 println!("Your guessed:{}",guess);
-	 match guess.cmp(&secret_number) {
-	 		 Ordering::Less => println!("Too small!"),
-			 Ordering::Greater => println!("Too big!"),
-			 Ordering::Equal=>println!("You win!"),
-	}
-	 
+		  let guess: u32 = match guess.trim().parse() {
+		  		Ok(num) => num,
+				Err(_) => continue,
+		  };
+
+		  println!("Your guessed:{}",guess);
+		  match guess.cmp(&secret_number) {
+		  		  Ordering::Less => println!("Too small!"),
+				  Ordering::Greater => println!("Too big!"),
+				  Ordering::Equal=>{
+					println!("You win!");
+					break;
+				}
+		}
+	} 
 }
