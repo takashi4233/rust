2f3eb827ffafc11b96115bd440a78405a1ae0f38
taksahi <takashi4233@gmail.com>
2021-02-22 20:19:00 +0900

    String型

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,20 +1,33 @@
 fn main() {
-	const MAX_POINTS: u32 = 100_000;
-	println!("MAX_POINTS is:{}",MAX_POINTS);
-	let mut x = 5;
-	println!("The value of x is:{}",x);
-	x = 6;
-	println!("The value of x is:{}",x);
 
-	// tapple
-	let tap = (1.0,"Apple",'A');
-	println!("tap.1 is {}",tap.1);
-	let (x,y,z) = tap;
-	println!("x is :{}",z);
+	 let mut c = "hoge!";
+	 println! ("word:{}",c);
+	 c = "page!";
+	 println! ("word:{}",c);
 
-	// array
-	let ary = ["apple","orange","grape"];
-	println!("ary[1] is {}",ary[1]);
+	 
+	 let mut s = String::from("hello");
+	 println! ("word:{}",s);
+	 s.push_str(", world!");
+	 println! ("word:{}",s);
+
+	 
+	 const MAX_POINTS: u32 = 100_000;
+	 println!("MAX_POINTS is:{}",MAX_POINTS);
+	 let mut x = 5;
+	 println!("The value of x is:{}",x);
+	 x = 6;
+	 println!("The value of x is:{}",x);
+
+	 // tapple
+	 let tap = (1.0,"Apple",'A');
+	 println!("tap.1 is {}",tap.1);
+	 let (_x,_y,z) = tap;
+	 println!("z is :{}",z);
+	 
+	 // array
+	 let ary = ["apple","orange","grape"];
+	 println!("ary[1] is {}",ary[1]);
 	
 
 }
